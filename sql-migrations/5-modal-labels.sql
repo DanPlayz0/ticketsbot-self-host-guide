720877cc91eb945d4033a5e4fb184e6254a08357
DanPlayz0 <damuffin36@gmail.com>
2025-09-03 11:20:04 -0700

    Add SQL Migration

---
+++ sql-migrations/5-modal-labels.sql
@@ -0,0 +1,2 @@
+-- Related to database update (https://github.com/TicketsBot-cloud/database/commit/e8e254858b76de7ac135d7145f78af96e13416c8)
+ALTER TABLE form_input ADD COLUMN description character varying (100)
